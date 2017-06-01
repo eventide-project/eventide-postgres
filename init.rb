949dd0a9fe34ccbcb6076c44235b228d5e530830
Scott Bellware <sbellware@gmail.com>
2017-05-31 19:32:49 -0500

    init.rb requires load_path.rb

--- init.rb
+++ init.rb
@@ -1,10 +1,3 @@
-lib_dir = File.expand_path('../lib', __FILE__)
-$LOAD_PATH.unshift lib_dir unless $LOAD_PATH.include?(lib_dir)
-
-libraries_dir = ENV['LIBRARIES_HOME']
-unless libraries_dir.nil?
-  libraries_dir = File.expand_path(libraries_dir)
-  $LOAD_PATH.unshift libraries_dir unless $LOAD_PATH.include?(libraries_dir)
-end
+require_relative './load_path'
 
 require 'eventide/postgres'
