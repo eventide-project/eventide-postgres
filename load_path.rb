f2229c821e9a9cda2a6fad1ffd23700db5c3ffe8
Scott Bellware <sbellware@gmail.com>
2018-08-13 19:54:57 -0500

    load_path.rb is updated with the latest version

--- load_path.rb
+++ load_path.rb
@@ -8,8 +8,8 @@ rescue LoadError
   Bundler.require
 end
 
-lib_dir = File.join('lib', __dir__)
-$LOAD_PATH.unshift lib_dir unless $LOAD_PATH.include?(lib_dir)
+lib_dir = File.expand_path('lib', __dir__)
+$LOAD_PATH.unshift(lib_dir) unless $LOAD_PATH.include?(lib_dir)
 
 libraries_dir = ENV['LIBRARIES_HOME']
 unless libraries_dir.nil?
